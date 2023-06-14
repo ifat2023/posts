f0ce8089ef234c95e76b66e86814aea0d12026eb
ifat2023 <ifat.elgart@gmail.com>
2023-06-14 09:24:27 +0300

    second commit

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then(response => response.json())
   .then(posts => {
-    // Display posts
     const postContainer = document.getElementById('post-container');
     posts.forEach(post => {
       const postElement = document.createElement('div');
-      postElement.innerHTML = 
-      `
+      postElement.innerHTML = `
         <h2>${post.title}</h2>
-        <div button type="button" onclick="fetchComments<p>('${post.body}')</p>"</button>
+        <button  class="btn" type="button">View Comments</button>
+        <div class="comments" id="comments-${post.id}"></div>
       `;
-  
+
       postElement.addEventListener('click', () => {
-        fetchComments(post.id);
+        const commentsContainer = document.getElementById(`comments-${post.id}`);
+        commentsContainer.innerHTML = '';
+
+        fetchComments(post.id)
+          .then(comments => {
+            comments.forEach(comment => {
+              const commentElement = document.createElement('div');
+              commentElement.innerHTML = `
+                <h3 id=colors>${comment.name}</h3>
+                <p id= comments.body>${comment.body}</p>
+              `;
+              commentsContainer.appendChild(commentElement);
+            });
+          })
+          .catch(error => console.log(error));
       });
+
       postContainer.appendChild(postElement);
     });
   })
   .catch(error => console.log(error));
 
-
 function fetchComments(postId) {
-  fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
-    .then(response => response.json())
-    .then(comments => {
-      // Display comments
-      const commentsContainer = document.getElementById('comments-container');
-      commentsContainer.innerHTML = '';
-      comments.forEach(comment => {
-        const commentElement = document.createElement('div');
-        commentElement.innerHTML = `
-          <h3>${comment.name}</h3>
-          <p>${comment.body}</p>
-        `;
-        commentsContainer.appendChild(commentElement);
-      });
-    })
-    .catch(error => console.log(error));
+  return fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+    .then(response => response.json());
 }
